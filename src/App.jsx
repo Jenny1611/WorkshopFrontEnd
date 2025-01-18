5ecfd60b713cb7300c6e3b333b38f5581a5ab430
giacomo-bearzi <giacomo.bearzi@gmail.com>
2025-01-18 15:45:50 +0100

    Forse ci siamo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import GroupCard from "./components/groupCard";
 import Navbar from "./components/navbar";
 import sampleImage from "./assets/sample-image.jpg";
-import Group1_1 from "./components/Group1/Group1-1/Group1-1";
-import Group1_2 from "./components/Group1/Group1-2/Group1-2";
-import Group1_3 from "./components/Group1/Group1-3/Group1-3";
-import Group_3 from "./components/Group3/Group3.jsx";
+import Group1 from "./components/Group1/Group1";
+import Group2 from "./components/Group2/Group2";
+import Group3 from "./components/Group3/Group3";
+import Description from "./components/Description/Description";
 
 const App = () => {
   return (
     <>
       <Navbar />
+
+      <Description />
+
       <div className="row mt-4 mx-4">
         <div className="col-4 mb-4">
           <GroupCard difficulty="easy" imageSrc={sampleImage} title="Easy" text={"Descrizione progetto"} />
@@ -22,11 +25,9 @@ const App = () => {
         </div>
       </div>
 
-      <Group1_1></Group1_1>
-      <Group1_2></Group1_2>
-      <Group1_3></Group1_3>
-
-      <Group_3 />
+      <Group1 />
+      <Group2 />
+      <Group3 />
     </>
   );
 };
